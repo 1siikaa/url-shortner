2e77a76530fe0fb60525ea25bf3065298ab9cd1f
1siikaa <srishtidwivedi672@gmail.com>
2023-03-15 12:28:52 +0530

    done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
 const route = require('./src/routes/route')
 app.use(express.json())
+const cors = require('cors')
 app.use(cors({
-origin :["http://localhost:3000", "https://url-shortner-orcin-ten.vercel.app"]
+   origin:[ "http://localhost:3000" , "https://main--adorable-croissant-6c39f3.netlify.app"  ]
 }))
 mongoose.connect('mongodb+srv://1siikaa07:1siikaa07@cluster0.8px4cpv.mongodb.net/url-shortener?retryWrites=true&w=majority',{
     useNewUrlParser: true
@@ -15,7 +15,9 @@ mongoose.connect('mongodb+srv://1siikaa07:1siikaa07@cluster0.8px4cpv.mongodb.net
     .catch(err => console.log(err))
 
 app.use('/', route)
-
+app.use('/*', function(req,res){
+    res.status(400).send("Provided url is wrong")
+})
 app.listen((3001), function () {
     console.log("running on port" ,3001)
 })
