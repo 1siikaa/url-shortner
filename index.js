7bb84e62a83aa9dfad517686e499ffbbcd4d2bea
1siikaa <srishtidwivedi672@gmail.com>
2023-03-15 11:44:10 +0530

    done

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const route = require('./src/routes/route')
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+origin :["http://localhost:3000", "https://url-shortner-orcin-ten.vercel.app"]
+}))
 mongoose.connect('mongodb+srv://1siikaa07:1siikaa07@cluster0.8px4cpv.mongodb.net/url-shortener?retryWrites=true&w=majority',{
     useNewUrlParser: true
 },mongoose.set('strictQuery', true))
@@ -13,6 +15,7 @@ mongoose.connect('mongodb+srv://1siikaa07:1siikaa07@cluster0.8px4cpv.mongodb.net
     .catch(err => console.log(err))
 
 app.use('/', route)
+
 app.listen((3001), function () {
     console.log("running on port" ,3001)
 })
