f35076ebf7e24490346ede379b9d1b666911bf7d
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2023-03-16 13:38:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const cors = require('cors')
 app.use(cors({
    origin:[ "http://localhost:3000" , "https://main--adorable-croissant-6c39f3.netlify.app/"  ]
 }))
+
 mongoose.connect('mongodb+srv://1siikaa07:1siikaa07@cluster0.8px4cpv.mongodb.net/url-shortener?retryWrites=true&w=majority',{
     useNewUrlParser: true
 },mongoose.set('strictQuery', true))
 
     .then(() => console.log("mongodb is connected"))
     .catch(err => console.log(err))
+
+app.use(function(req, res, next) {
+ res.header('Access-Control-Allow-Origin', 'https://main--adorable-croissant-6c39f3.netlify.app/');
+ res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+ res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+ res.header('Access-Control-Allow-Credentials', 'true');
+ next();
+ });
 
 app.use('/', route)
 app.use('/*', function(req,res){
